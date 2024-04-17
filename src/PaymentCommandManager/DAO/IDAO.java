791f8c51d47e52ff34bb07901080e6d429826f9e
Imad Asserne <imad.asserne@gmail.com>
2024-04-17 14:29:28 +0000

    change type of retrun

--- src/PaymentCommandManager/DAO/IDAO.java
+++ src/PaymentCommandManager/DAO/IDAO.java
@@ -1,8 +1,10 @@
 package PaymentCommandManager.DAO;
 
+import java.util.ArrayList;
+
 public interface IDAO<T> {
     int insert(T Obj);
-    int delete(int id);
-    String update(String sql);
-    String select();
+    int delete(T Obj);
+    int  update(T Obj);
+    ArrayList<T> select(T Obj);
 }
